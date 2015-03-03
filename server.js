0a38f89cc7e38f209cd3375a026f30f1dbd19d3e
Marian Ignev <m.ignev@gmail.com>
2015-03-03 22:32:12 +0200

    asd

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
 
+var server_ip    = process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    server_port  = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(server_port, server_ip, function () {
 
   var host = server.address().address
   var port = server.address().port
